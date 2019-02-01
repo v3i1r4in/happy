2f7aafa361d51ef4c0081f7f7b9a9da0b0d94e1b
David Cao <tianchic@zillowgroup.com>
2019-02-01 11:43:23 -0800

    s

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('http').createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('<h1>&#128512; happy hackweek!</h1>');
+    response.end('<h1>&#128512; hello there!</h1>');
 })
 .listen(8080)
